46d4286bc70abbe0d2ca0948fef408a1b865dff9
吴佳浩 <wujiahao@supremind.com>
2024-11-21 16:42:08 +0800

    加入日志文件方便查看记录接入下级平台正常  注册上级平台正常

---
+++ 稳定文件版本/main.go
@@ -0,0 +1,36 @@
+package mainbackup
+
+import (
+	"28181sip/common"
+	"28181sip/gb28181"
+)
+
+func main() {
+	common.Info("Starting application...")
+	/**
+	 * 1.对接下级设备获取设备列表
+	 */
+	// gb28181.Getdevice("udp", "100.100.155.157:5060")
+	/**
+	 * 2.对接上级平台级设备获取设备列表
+	 */
+	gb28181.SimulateNVR("conf/config.yaml")
+	/**
+	 * 3.对接上级平台级请求
+	 */
+	// buffer := make([]byte, 4096)
+	// conn := gb28181.Conn
+	// for {
+	// 	n, remoteAddr, err := conn.ReadFromUDP(buffer)
+	// 	if err != nil {
+	// 		common.Errorf("接收消息时出错: %v", err)
+	// 		return
+	// 	}
+	// 	message := string(buffer[:n])
+	// 	message = gb28181.CleanSIPMessage(message) // 添加这行
+	// 	err = gb28181.HandleSIPRequest(conn, message, remoteAddr)
+	// 	if err != nil {
+	// 		common.Errorf("处理SIP请求时出错: %v", err)
+	// 	}
+	// }
+}
